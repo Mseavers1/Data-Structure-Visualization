2f1c415570fcbf1d15618b6f7cb85f1600076202
Mseavers1 <91432334+Mseavers1@users.noreply.github.com>
2024-10-13 17:48:44 -0500

    Started animation site

--- client/src/components/algorithms/arrays.js
+++ client/src/components/algorithms/arrays.js
@@ -1,9 +1,29 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
+
 export default function Arrays() {
     return (
         <div className="animations">
 
             <div className="animations-header">
+                <TextField
+                    id="filled-search"
+                    label="Input"
+                    type="search"
+                    variant="filled"
+                    sx={{
+                        scale: "90%",
+                        width: "100px",
+                        backgroundColor: "#ffffff",
+                        color: "#000",
+                    }}
+                />
 
+                <Button variant="contained" sx={{ backgroundColor: '#b01e24', color: '#ffffff ', }}>Add</Button>
+                <Button variant="contained" sx={{ backgroundColor: '#b01e24', color: '#ffffff ', }}>Delete</Button>
+                <Button variant="contained" sx={{ backgroundColor: '#b01e24', color: '#ffffff ', }}>Clear</Button>
             </div>
 
 
