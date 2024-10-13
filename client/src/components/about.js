30b0a566c7e6751203b5132c7249cb34726ad88c
Mseavers1 <91432334+Mseavers1@users.noreply.github.com>
2024-10-13 15:23:59 -0500

    Started array animation

--- client/src/components/about.js
+++ client/src/components/about.js
@@ -1,6 +1,6 @@
 export default function About() {
     return (
-        <div>
+        <div className="about">
             <h1>
                 Introduction
             </h1>
--- client/src/components/algorithms/arrays.js
+++ client/src/components/algorithms/arrays.js
@@ -1,7 +1,20 @@
 export default function Arrays() {
     return (
-        <div>
-            Test
+        <div className="animations">
+
+            <div className="animations-header">
+
+            </div>
+
+
+            <div className="animations-main">
+                Test
+            </div>
+
+
+            <div className="animations-footer">
+
+            </div>
         </div>
     );
 }
